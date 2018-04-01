84e4d78683190b343bde1bf2f334192eac0de17b
cwy <cwy@DESKTOP-0LU2V2F>
2018-04-01 22:22:07 +0800

    to identify the report file

--- seleniumCucumberDemo/src/test/java/seleniumCucumberDemo/seleniumCucumberDemo/TestingSelenium.java
+++ seleniumCucumberDemo/src/test/java/seleniumCucumberDemo/seleniumCucumberDemo/TestingSelenium.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin={"pretty"}, features={"src/test/features"})
+@CucumberOptions(plugin={"pretty","html:target/cucumber", "json:target/cucumber.json"}, features={"src/test/features"})
 
 public class TestingSelenium {
 
